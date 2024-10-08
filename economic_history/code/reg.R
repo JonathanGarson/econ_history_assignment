fc03cb73e8708830e31c4dd3a134cb8a5c9d80f1
JonathanGarson <jonathan.garson@sciencespo.fr>
2024-10-08 15:34:31 +0000

    finalized

--- economic_history/code/reg.R
+++ economic_history/code/reg.R
@@ -8,20 +8,49 @@ library(data.table)
 library(fixest)
 library(ggplot2)
 library(dplyr)
+library(stargazer)
+library(sandwich)
 
 # Import Data -------------------------------------------------------------
 cars = read_parquet('../data/final/clean_data.parquet')
 list_states = fread('../data/final/exposed_state.csv')
 
-# Regression --------------------------------------------------------------
+# Creating a new time fix effect -----------------------------------------
 
-reg_1 = feols(log(cars_sales) ~ i(exposure, post_crash), fixef = c("month","year" ,"state"), data = cars)
-summary(reg_1)
+cars = cars %>% 
+  mutate(timefe = as.numeric(paste0(
+    as.character(year), 
+    ifelse(nchar(as.character(month)) == 1, paste0("0", as.character(month)), as.character(month))
+  ), .after = state)) %>%
+  arrange(state, timefe)
+
+value_exposure_total = list_states[state == "Total", exposure]
+
+# Regression --------------------------------------------------------------
+#Adjust reference levels - Average as reference level
+cars$state <- factor(cars$state, 
+                     levels = c("Total", grep("Total", unique(list_states$state), value = TRUE, invert = TRUE)))
+
+reg_2 = lm(log(cars_sales) ~ exposure * post_crash + factor(timefe) + state -1, data = cars)
+
+lm_1_se <- sqrt(diag(vcovHC(reg_2, type = "HC3")))
+
+stargazer(reg_2,
+          type = "text", 
+          se = list(lm_1_se),
+          title = "Regression Results for Panel Data baseline model",
+          omit = c("state", "timefe"),
+          align = TRUE)
+
+summary(reg_2)
+
+# Effect by State
+reg_1 = feols(log(cars_sales) ~ i(exposure, post_crash), fixef = c("timefe","state"),data = cars)
 
 retrieve_result = function(reg){
   summary = summary(reg)
   tables = data.frame(
-    exposure = gsub("[^0-9.]", "",, x = names(summary$coefficients)),
+    exposure = as.numeric(gsub("[^0-9.-]", "", x = names(summary$coefficients))),
     estimate = summary$coefficients,
     sd = summary$se
   )
@@ -34,7 +63,7 @@ result = result %>%
 result = merge(result, list_states, by = "exposure", all.x = T)
 setorderv(result, cols = c("exposure"))
 
-ggplot(result, aes(x = estimate, y = state)) +  
+ggplot(result, aes(x = estimate, y = reorder(state, exposure))) +  
   geom_pointrange(aes(xmin = estimate_low, xmax = estimate_high), color = "blue") +
   geom_vline(xintercept = 0, linetype = "dashed", color = "red") + 
   labs(
@@ -45,9 +74,37 @@ ggplot(result, aes(x = estimate, y = state)) +
   ) +
   theme_minimal() +
   theme(
-    plot.caption = element_text(hjust = 0, size = 10, face = "italic")  # Right-align and italicize caption
+    plot.caption = element_text(hjust = 0, size = 10, face = "italic")  # Left-align and italicize caption
   )
 ggsave(filename = "../krach29carsales.pdf", height = 10, width = 7)
+
+# Bonus -------------------------------------------------------------------
+
+cars[, distance_treat := (year - 1929) * 12 + (month - 10)]
+cars_et= cars[distance_treat  %in% -9:24,]
+cars_et[, exposure_perc := exposure * 100]
+
+bonus_reg = feols(log(cars_sales) ~ i(distance_treat, ref = 0, exposure_perc), fixef = c("timefe", "state"), 
+                  data = cars_et, se = "hetero")
+summary(bonus_reg)
+result_bonus = retrieve_result(bonus_reg)
+result_bonus = result_bonus %>% 
+  mutate(estimate_low = estimate - 1.96 * sd,
+         estimate_high = estimate + 1.96 * sd)
+
+# Create the plot using estimates_low for both x and y
+ggplot(result_bonus, aes(x = exposure, y = estimate)) +
+  geom_point(color = "blue") +  # Add points for the estimated coefficients
+  # geom_line(color = "blue")+
+  geom_hline(yintercept = 0, linetype = "solid", color = "black")+
+  geom_vline(xintercept = 0, linetype = "dashed", color = "red") +  # Vertical line at the treatment point (e.g., October 1929)
+  geom_errorbar(aes(ymin = estimate_low , ymax = estimate_high), width = 0.2, color = "blue") +  # 95% CI
+  labs(title = "Event Study: Impact of Treatment on Car Sales",
+       x = "Distance to Treatment (Months)",
+       y = "Coefficient Estimate Exposure*MD") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave(filename = "../et_car_sales.pdf", height = 8, width = 10)
 
 # having fun --------------------------------------------------------------
 # 
